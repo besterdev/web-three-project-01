5b41984787a3e1e154d6d83942fe77542e1d3a84
Boy <thawatchai.kra@innovasive.co.th>
2022-03-13 22:33:27 +0700

    init project login flow with moralis completed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  publicRuntimeConfig: {
+    APP_ID: process.env.NEXT_PUBLIC_APP_ID,
+    SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+  },
+  images: {
+    domains: ["avatars.dicebear.com", "links.papareact.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { MoralisProvider } from "react-moralis";
+
+import getConfig from "next/config";
+
+const { publicRuntimeConfig } = getConfig();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      appId={publicRuntimeConfig.APP_ID}
+      serverUrl={publicRuntimeConfig.SERVER_URL}
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
